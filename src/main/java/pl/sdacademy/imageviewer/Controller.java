3f1598f8712fb620756769a8daa6dd557892b981
Piotr <p-czajkowski@outlook.com>
2018-03-25 14:05:50 +0200

    Stworzenie widoku i dodanie do niego kontrolki do wyświetlania obrazów (imageViewer)

---
+++ src/main/java/pl/sdacademy/imageviewer/Controller.java
@@ -0,0 +1,12 @@
+package pl.sdacademy.imageviewer;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ImageView imageViewer;
+
+
+}
--- src/main/java/pl/sdacademy/imageviewer/Main.java
+++ src/main/java/pl/sdacademy/imageviewer/Main.java
@@ -1,4 +1,21 @@
 package pl.sdacademy.imageviewer;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        primaryStage.setTitle("Przeglądarka obrazów");
+        primaryStage.setScene(new Scene(root, 1024, 1024));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
