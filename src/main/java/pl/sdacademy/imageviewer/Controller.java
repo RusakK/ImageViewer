1fe819ef920bf852b4971fca4c6a183f194cfa9c
Krzysztof Rusak <krzysztofrusak91@gmail.com>
2018-03-25 14:34:42 +0200

    otworzenie okna dialogowego po kliknieciu przycisku

--- src/main/java/pl/sdacademy/imageviewer/Controller.java
+++ src/main/java/pl/sdacademy/imageviewer/Controller.java
@@ -1,10 +1,18 @@
 package pl.sdacademy.imageviewer;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+
 
     @FXML
     private ImageView imageViewer;
@@ -13,6 +21,33 @@ public class Controller {
     @FXML
     private Button buttonRight;
 
+    @FXML
+    private Button buttonOpen;
 
+
+    private Stage stage;
+
+    public void initialize(){
+
+
+        buttonOpen.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+                /*final DirectoryChooser directoryChooser =
+                        new DirectoryChooser();
+
+                final File selectedDirectory =
+                        directoryChooser.showDialog(stage);
+                if (selectedDirectory != null) {
+                    selectedDirectory.getAbsolutePath();*/
+
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open file");
+                fileChooser.showOpenDialog(stage);
+            }
+        });
+
+
+    }
 
 }
