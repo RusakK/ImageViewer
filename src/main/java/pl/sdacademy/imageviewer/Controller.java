88fae1bbc0ed227e826dc260b421cc231cca5f7a
Piotr <p-czajkowski@outlook.com>
2018-03-25 14:13:10 +0200

    Dodanie do widoku kontrolki w prawo i w lewo

--- src/main/java/pl/sdacademy/imageviewer/Controller.java
+++ src/main/java/pl/sdacademy/imageviewer/Controller.java
@@ -1,12 +1,18 @@
 package pl.sdacademy.imageviewer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 public class Controller {
 
     @FXML
     private ImageView imageViewer;
+    @FXML
+    private Button buttonLeft;
+    @FXML
+    private Button buttonRight;
+    
 
 
 }
