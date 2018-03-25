c54b41d891873c7ecf7d2e9f476584aae9ff1a8d
Krzysztof Żurada <kzurada@gmail.com>
2018-03-25 13:45:29 +0200

    dodanie klasy ImageFileOpener

---
+++ src/main/java/pl/sdacademy/imageviewer/ImageFileOpener.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.imageviewer;
+
+import java.util.List;
+
+public class ImageFileOpener {
+    private List<ImageFileOpener> imageFileOpeners;
+    private int currentFileIndex;
+}
